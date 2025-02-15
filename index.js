d8073052cdb101a384393f17cf84124b10f37503
CodeKhushal <khushalsachdeva22@gmail.com>
2025-02-15 13:46:45 +0530

    Fixed cors issue

--- index.js
+++ index.js
@@ -8,25 +8,35 @@ import cors from "cors";
 
 dotenv.config({});
 
-
 const app = express();
 const PORT = process.env.PORT || 3000;
+const whitelist = [
+  "https://kexptrack.vercel.app",
+  "http://localhost:5173",
+  "https://expense-tracker-frontend-seven-alpha.vercel.app",
+];
 
 // middleware
 app.use(express.json());
-app.use(urlencoded({extended:true}));
+app.use(urlencoded({ extended: true }));
 app.use(cookieParser());
 const corsOptions = {
-    origin:"https://kexptrack.vercel.app",
-    credentials:true
-}
+  function(origin, callback) {
+    if (whitelist.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  credentials: true,
+};
 app.use(cors(corsOptions));
 
 // api's
 app.use("/api/v1/user", userRoute);
 app.use("/api/v1/expense", expenseRoute);
 
-app.listen(PORT, ()=>{
-    connectDB();
-    console.log(`Server listen at port ${PORT}`);
-})
+app.listen(PORT, () => {
+  connectDB();
+  console.log(`Server listen at port ${PORT}`);
+});
